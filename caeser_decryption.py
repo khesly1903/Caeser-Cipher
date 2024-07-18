82889f1caee3318afb87a0833d220cdfdf1f71bd
Berkay Kaya <98612716+khesly1903@users.noreply.github.com>
2024-07-19 01:11:30 +0300

    Update caeser_decryption.py

--- caeser_decryption.py
+++ caeser_decryption.py
@@ -7,16 +7,17 @@ for i in range(0,26):
     for j in range(len(msg)):
         msg_arr.append(msg[j])
         
-        if ord(msg_arr[j]) in range(65,91):
+        if ord(msg_arr[j]) in range(65,91): #uppercase letters
             normal = ord(msg_arr[j]) - 65
             shifted = (normal + i ) % 26 + 65
             dec_msg.append(chr(shifted))
         
-        elif ord(msg_arr[j]) in range(97,123):
+        elif ord(msg_arr[j]) in range(97,123): #lowecase letters
             normal = ord(msg_arr[j]) - 97
             shifted = (normal + i ) % 26 + 97
-            dec_msg.append(chr(shifted))                
-        if ord(msg_arr[j]) == 32:
+            dec_msg.append(chr(shifted))     
+            
+        if ord(msg_arr[j]) == 32: #space
             dec_msg.append(msg_arr[j])
 
     
